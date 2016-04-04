f09cc6786ec3f8a053966cdf170bd16ab80f95e3
apkkids <apkkids@163.com>
2016-04-04 19:54:58 +0800

    add HashMapUsage.

---
+++ src/com/dumaisoft/collection/HashMapUsage.java
@@ -0,0 +1,11 @@
+package com.dumaisoft.collection;
+
+/**
+ * Author:      wxb
+ * Project:     JavaExampleCode
+ * Create Date: 2016/4/4
+ * Create Time: 19:52
+ * Description:
+ */
+public class HashMapUsage {
+}
