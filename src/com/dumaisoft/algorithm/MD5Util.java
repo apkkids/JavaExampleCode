b602f2547f9cdd6d28ff44d36d1bc10f28b087f0
apkkids <apkkids@163.com>
2016-05-19 21:27:38 +0800

    add pakage algorithm, add MD5Util.java

---
+++ src/com/dumaisoft/algorithm/MD5Util.java
@@ -0,0 +1,51 @@
+package com.dumaisoft.algorithm;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Author:      wxb
+ * Project:     JavaExampleCode
+ * Create Date: 2016/5/19
+ * Create Time: 21:19
+ * Description: get the md5 code from a string.
+ */
+public class MD5Util {
+    private final static String[] strDigits = {"0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+
+    private static String byteToArrayString(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        int iD1 = iRet / 16;
+        int iD2 = iRet % 16;
+        return strDigits[iD1] + strDigits[iD2];
+    }
+
+    private static String byteToString(byte[] bByte) {
+        StringBuffer sBuffer = new StringBuffer();
+        for (int i = 0; i < bByte.length; i++) {
+            sBuffer.append(byteToArrayString(bByte[i]));
+        }
+        return sBuffer.toString();
+    }
+
+    public static String GetMD5Code(String strObj) {
+        String resultString = null;
+        try {
+            resultString = new String(strObj);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            resultString = byteToString(md.digest(strObj.getBytes()));
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+        return resultString;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(GetMD5Code("000000"));
+        System.out.println(GetMD5Code("000001"));
+    }
+}
