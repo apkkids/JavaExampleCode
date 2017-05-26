51044cec40edd17e3df4268bcd6616d798f94bf6
apkkids <apkkids@163.com>
2017-05-26 22:32:12 +0800

    add concurrency code.

--- src/com/dumaisoft/concurrency/ConcurrentLinkedQueueExam.java
+++ src/com/dumaisoft/concurrency/ConcurrentLinkedQueueExam.java
@@ -1,11 +1,8 @@
 package com.dumaisoft.concurrency;
 
 import java.util.LinkedList;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 /**
  * Author:      wxb
@@ -15,56 +12,89 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Description:
  */
 public class ConcurrentLinkedQueueExam {
+    private static final int TEST_INT = 10000000;
+
     public static void main(String[] args) throws InterruptedException {
-        ConcurrentLinkedQueue<Integer> queue = new ConcurrentLinkedQueue<>();
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        long start = System.currentTimeMillis();
+        Queue<Integer> queue = null;
+        if (args.length < 1) {
+            System.out.println("Usage: input 1~4 ");
+            System.exit(1);
+        }
+        int param = Integer.parseInt(args[0]);
+        switch (param) {
+            case 1:
+                queue = new LinkedList<>();
+                break;
+            case 2:
+                queue = new LinkedBlockingQueue<>();
+                break;
+            case 3:
+                queue = new ArrayBlockingQueue<Integer>(TEST_INT * 5);
+                break;
+            case 4:
+                queue = new ConcurrentLinkedQueue<>();
+                break;
+            default:
+                System.out.println("Usage: input 1~4 ");
+                System.exit(2);
+        }
+        System.out.println("Using " + queue.getClass().getSimpleName());
+
         ExecutorService service = Executors.newCachedThreadPool();
-        for (int i = 0; i < 2; i++) {
-            service.submit(new Putter(linkedList));
+        for (int i = 0; i < 5; i++) {
+            service.submit(new Putter(queue, "Putter" + i));
         }
         TimeUnit.SECONDS.sleep(2);
-        for (int i = 0; i < 2; i++) {
-            service.submit(new Getter(linkedList));
+        for (int i = 0; i < 5; i++) {
+            service.submit(new Getter(queue, "Getter" + i));
         }
         service.shutdown();
-    }
-}
-
-class Putter implements Runnable {
-    private final LinkedList<Integer> linkedList;
-    private static AtomicInteger atomicInteger = new AtomicInteger(0);
-
-    Putter(LinkedList<Integer> linkedList) {
-        this.linkedList = linkedList;
+        service.awaitTermination(1, TimeUnit.DAYS);
+        long end = System.currentTimeMillis();
+        System.out.println("Time span = " + (end - start));
+        System.out.println("queue size = " + queue.size());
     }
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 1000; i++) {
-            linkedList.add(i);
-            atomicInteger.incrementAndGet();
+    static class Putter implements Runnable {
+        private final Queue<Integer> queue;
+        private final String name;
+
+        Putter(Queue<Integer> queue, String name) {
+            this.queue = queue;
+            this.name = name;
         }
-        System.out.println("Putter=" + atomicInteger.get());
-    }
-}
 
-class Getter implements Runnable {
-    private final LinkedList<Integer> linkedList;
-    private static AtomicInteger result = new AtomicInteger(0);
 
-    Getter(LinkedList<Integer> linkedList) {
-        this.linkedList = linkedList;
-    }
-
-    @Override
-    public void run() {
-        while (!linkedList.isEmpty()) {
-            linkedList.poll();
-            result.incrementAndGet();
-            if (result.get() % 100 == 0) {
-                System.out.println("Getter=" + result);
+        @Override
+        public void run() {
+            for (int i = 0; i < TEST_INT; i++) {
+                queue.offer(1);
             }
+            System.out.println(name + " is over");
         }
-        System.out.println("Getter=" + result);
+    }
+
+    static class Getter implements Runnable {
+        private final Queue<Integer> queue;
+        private final String name;
+
+        Getter(Queue<Integer> queue, String name) {
+            this.queue = queue;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < TEST_INT; i++) {
+                synchronized (Getter.class) {
+                    if (!queue.isEmpty()) {
+                        queue.poll();
+                    }
+                }
+            }
+            System.out.println(name + " is over");
+        }
     }
 }
+
